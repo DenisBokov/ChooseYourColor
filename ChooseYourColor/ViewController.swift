a61705cce5e4f96b4ef7eadf47edd83a789d7440
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-05-30 13:55:58 +0300

    Added layout and links to the ViewController

--- ChooseYourColor/ViewController.swift
+++ ChooseYourColor/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ColorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeRedColor() {
+    }
+    
+    @IBAction func changerGreenColor() {
+    }
+    
+    @IBAction func changeBlueColor() {
+    }
+    
+    
 }
 
